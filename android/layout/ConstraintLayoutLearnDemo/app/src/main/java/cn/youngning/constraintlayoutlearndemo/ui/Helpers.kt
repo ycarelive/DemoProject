bf783e6bb3d8673a8ad01bce438da6b9cd504eef
Yning <768305195@qq.com>
2020-09-13 20:03:29 +0800

    新增约束布局Demo

---
+++ android/layout/ConstraintLayoutLearnDemo/app/src/main/java/cn/youngning/constraintlayoutlearndemo/ui/Helpers.kt
@@ -0,0 +1,31 @@
+package cn.youngning.constraintlayoutlearndemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.widget.Group
+import cn.youngning.constraintlayoutlearndemo.R
+
+class Helpers : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<Group>(R.id.group).visibility = View.GONE
+        }
+
+
+//        setContentView(R.layout.activity_layer)
+//        findViewById<Button>(R.id.button).setOnClickListener {
+//            findViewById<Layer>(R.id.layer).rotation = 45f
+//            findViewById<Layer>(R.id.layer).translationY = 100f
+//            findViewById<Layer>(R.id.layer).translationX = 100f
+//        }
+//
+        //setContentView(R.layout.activity_barrier)
+
+    }
+}
